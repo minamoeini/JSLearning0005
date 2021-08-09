fa6a0638f12ac761d16c31b83dddd8538964be13
ali edalat <aliedalat.pr@gmail.com>
2021-08-09 23:21:34 +0430

    after session 3 000518

---
+++ app3-2.js
@@ -0,0 +1,96 @@
+const root = document.getElementById('myspan');
+
+
+// const third = document.querySelectorAll("#myspan > .c3");
+// third[0].style.color = "green";
+//span.innerHTML = "<div>JIJIJ</div>";
+//span.innerText = "<div>JIJIJ</div>";
+//span.setAttribute("id","NEWID");
+//span.style.color = 'red';
+//span.style = "color:red";
+//span.setAttribute("style","color:red");
+//span.style.color = 'red';
+
+const c1 = document.querySelectorAll('#myspan > .c1')[0];
+const c2 = document.querySelectorAll('#myspan > .c2')[0];
+
+const myol = document.createElement('ol',{
+    id:'ol1'
+});
+
+const li1  = document.createElement('li');
+li1.innerText = "List Item 1";
+
+myol.appendChild(li1);
+
+//root.replaceChild(myol,c1);
+root.removeChild(c2);
+root.insertBefore(myol,c1);
+
+//c1.setAttribute("onclick","alert('y');");
+//document.write("dasdasd");
+
+// function handleClickC1(){
+//     alert('AAA');
+// }
+function handleClickC1(){
+    
+    alert('AAA');
+}
+function handleClickC1_2(){
+    alert('AAA 2');
+}
+
+//c1.onclick = handleClickC1;
+
+c1.addEventListener('click',handleClickC1);
+c1.addEventListener('click',handleClickC1_2);
+
+//c1.removeEventListener('click',handleClickC1);
+
+const innerDiv = document.getElementById('innerDiv');
+innerDiv.style.width = "100px";
+innerDiv.style.height = "100px";
+innerDiv.style.backgroundColor = "blue";
+
+root.style.border = "2px solid yellow";
+
+innerDiv.onclick = function(){
+    console.log("Blue Clicked");
+}
+
+// root.onclick=function(){
+//     console.log("Root Clicked");
+// }
+
+// root.addEventListener('click',function(){
+//         console.log("Root Clicked");
+//     },{
+//         capture:true
+//     });
+
+root.addEventListener('click',function(event){
+    event.stopPropagation();
+    console.log("Root Clicked");
+},{
+    capture:true
+});
+
+const innerDiv2 = document.createElement('div');
+innerDiv2.style.width = "80px";
+innerDiv2.style.height = "80px";
+innerDiv2.style.backgroundColor = "red";
+innerDiv.appendChild(innerDiv2);
+
+// innerDiv2.onclick = function(){
+//     console.log("Red  Clicked");
+// }
+
+innerDiv2.addEventListener('click',function(){
+    console.log("Red Clicked");
+},{
+    capture:true
+});
+
+
+console.log(myol);
---
+++ app3.js
@@ -0,0 +1,102 @@
+//const arr = ["ali","reza","hasan","azar"];
+
+// const arrObj = [
+//     {name:'ali',age:12},
+//     {name:'reza',age:18},
+//     {name:'hasan',age:20},
+//     {name:'azar',age:15}
+// ];
+
+// function mysort(a,b){
+//     if (a.name > b.name) return -1;
+//     if (a.name < b.name) return 1;
+//     return 0;
+// }
+
+// arrObj.sort(mysort);
+
+// console.log(arrObj);
+
+//const arr = ["ali","reza","hasan","azar"];
+
+// const newArr = arr.map(function (element){
+//     return ". First Name Is: " + element;
+// });
+
+// console.log(newArr);
+
+// const arrObj = [
+//     {name:'ali',age:12},
+//     {name:'reza',age:18},
+//     {name:'hasan',age:20},
+//     {name:'azar',age:15}
+// ];
+
+// let sumAge = 0;
+
+// arrObj.forEach(function(element,index){
+//     element.age += 2;
+// });
+
+
+// console.log(arrObj);
+//console.log(sumAge / arrObj.length);
+
+
+// const result = arrObj.every(function(element){
+//     return element.age < 22;
+// });
+
+// const result = arrObj.some(function(element){
+//     return element.age < 10;
+// });
+
+// console.log(result);
+
+const arrObj = [
+    {name:'ali',age:12},
+    {name:'reza',age:18},
+    {name:'hasan',age:20},
+    {name:'hasan',age:23},
+    {name:'azar',age:15}
+];
+
+// const index = arrObj.findIndex(function(element,index){
+//     return element.name === "hasan";
+// });
+
+// console.log(index);
+
+// arrObj[index].age -=2;
+
+// const element = arrObj.find(function(element,index){
+//     return element.name === "hasan";
+// });
+
+// console.log(element);
+
+// const filtered = arrObj.filter(function(element,index){
+//     return element.name === "azar";
+// });
+
+// const finded = arrObj.find(function(element,index){
+//     return element.name === "azar";
+// });
+
+// console.log(finded);
+// console.log(filtered[0]);
+
+// const result = arrObj.reduce(function(prev,current){
+//     return prev.age + current.age;
+// });
+
+// console.log(result);
+
+// const date = new Date(2021,12,3);
+// console.log(date.get);
+
+// const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
+
+// console.log(pattern.test("ali@gma.com"));
+
+
